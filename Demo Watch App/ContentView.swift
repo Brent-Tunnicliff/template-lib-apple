54e1975cf16ef380c20f077c1f56c374c6e7228a
Brent Tunnicliff <brent@tunnicliff.dev>
2025-03-25 23:02:58 +1100

    Brought across changes made as part of the lib-logging-apple build.

---
+++ Demo Watch App/ContentView.swift
@@ -0,0 +1,19 @@
+// Copyright © 2025 Brent Tunnicliff <brent@tunnicliff.dev>
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo Watch App/DemoApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2025 Brent Tunnicliff <brent@tunnicliff.dev>
+
+import SwiftUI
+
+@main
+struct Demo_Watch_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
